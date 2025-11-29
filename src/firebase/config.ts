7be042dc29c29ddcf1dcdf7ca2ad02f50b03bd85
Thiruvengala Brahmani Mandru <thiruvengalabrahmanimandru@gmail.com>
2025-11-29 20:31:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5570530623-19b46",
+  "appId": "1:102512018330:web:6f7da955cfd0a1c546f023",
+  "apiKey": "AIzaSyBF120ca8APcV2TM83KZRsdh3LOIXzIc2c",
+  "authDomain": "studio-5570530623-19b46.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "102512018330"
+};
